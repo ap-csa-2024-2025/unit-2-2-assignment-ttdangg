5210b8f85be6318abfa644920f4838068513fff7
ttdangg <ttdang1445@gmail.com>
2024-10-01 10:16:47 -0700

    shopping

--- Classwork.java
+++ Classwork.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
+
 public class Classwork
 {
   public static void main(String[] args)
   {
-    // write your code
+    Scanner sc = new Scanner(System.in);
+    System.out.println("What type of item are you buying? ");
+    String item = sc.nextLine();
+    System.out.println("How many are you buying?");
+    int number_of_product = sc.nextInt();
+    System.out.println("How much does each one weigh?");
+    double weight = sc.nextDouble();
+
+    double total_weight = number_of_product * weight;
+    System.out.println(total_weight);
   }
 }
